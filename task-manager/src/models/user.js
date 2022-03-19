ebd9ec24cd2ce637b4a305d4074033c56467f33e
YunusKaratepe <yunus.karatepe1699@gmail.com>
2022-03-19 23:30:38 +0300

    env variables

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -81,7 +81,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
 userSchema.methods.generateAuthToken = async function() {
     const user = this
 
-    const token = jwt.sign({ _id: user._id.toString() }, 'thisisascretkey')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET_KEY)
     user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
